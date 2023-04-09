49aa24dbca8e4d1d7a30e68fcd9703198953d447
kevin bogunovic <bogunovic_kevin@hotmail.com>
2023-04-09 18:42:45 +0100

    added theme change button and javascript

---
+++ javascript/script.js
@@ -0,0 +1,23 @@
+const body = document.querySelector('body');
+const headings = document.querySelectorAll('.h4');
+const navLinks = document.querySelectorAll('.nav-link');
+const themeToggle = document.getElementById('theme-toggle');
+const themeStatus = document.querySelector('.theme-status');
+
+function toggleTheme() {
+  for (i = 0; i < headings.length; ++i) {
+  headings[i].classList.toggle('h4-light-mode');
+  }
+  for (i = 0; i < navLinks.length; ++i) {
+  navLinks[i].classList.toggle('h4-light-mode');
+  }
+  body.classList.toggle('light-mode');
+  const themeMessage = body.classList.contains('light-mode') ?
+    'Dark'
+    : "Light"
+  themeStatus.innerText = themeMessage; 
+}
+  
+themeToggle.addEventListener('click', () => {
+  toggleTheme();
+}); 
